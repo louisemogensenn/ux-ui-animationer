c40c46475c92e487e1eb6156fb0a674f58f4446a
ClaireKahindi <160113374+ClaireKahindi@users.noreply.github.com>
2024-10-08 13:27:47 +0200

    CTA knapper
    omvendt funktion af CTA-knapper

--- omos.js
+++ omos.js
@@ -12,13 +12,12 @@ window.addEventListener('scroll', () => {
 
 
 // Forsøg med CTA knapper
-
-const indsend = document.querySelector(".indsend");
-indsend.addEventListener("click",()=> {
-    indsend.classList.add("submiting");
-    indsend.innerHTML="";
-    setTimeout(()=>{
-        indsend.classList.remove("submiting");
-        indsend.innerHTML = "SENDT!";
-    },3000)
+const indsend = document.querySelector(".indsend"); // Vælger knappen med klassen 'indsend' og gemmer den i variablen 'indsend'
+indsend.addEventListener("click", function(){ //Tilføjer klik-'event'
+    indsend.classList.add("submiting"); // Når knappen klikkes, tilføjes klassen 'submiting' for at starte animationen
+    setTimeout(function(){ 
+        indsend.classList.remove("submiting"); //Fjerner submiting igen for at stoppe animationen
+        indsend.innerHTML = "SENDT!"; // Ændrer teksten til "SENDT"!
+        indsend.style.backgroundColor = "#7B0005"; //Ændrer baggrundsfarven
+    },1000) // Sætter timeout varigheden til 1 sek
 });
