bd3f4ec04b26cbf205460c0a06bed99d4df78024
ClaireKahindi <160113374+ClaireKahindi@users.noreply.github.com>
2024-10-09 10:57:38 +0200

    forsog med CTA forside

--- omos.js
+++ omos.js
@@ -10,3 +10,14 @@ indsend.addEventListener("click", function(){ //Tilføjer klik-'event'
         indsend.style.backgroundColor = "#7B0005"; //Ændrer baggrundsfarven
     },1000) // Sætter timeout varigheden til 1 sek
 });
+
+
+const knap = document.querySelector(".knap-forside"); // Vælger knappen med klassen 'indsend' og gemmer den i variablen 'indsend'
+knap.addEventListener("click", function(){ //Tilføjer klik-'event'
+    knap.classList.add("submiting"); // Når knappen klikkes, tilføjes klassen 'submiting' for at starte animationen
+    setTimeout(function(){ 
+        knap.classList.remove("submiting"); //Fjerner submiting igen for at stoppe animationen
+        knap.innerHTML = "SENDT!"; // Ændrer teksten til "SENDT"!
+        knap.style.backgroundColor = "#7B0005"; //Ændrer baggrundsfarven
+    },1000) // Sætter timeout varigheden til 1 sek
+});
